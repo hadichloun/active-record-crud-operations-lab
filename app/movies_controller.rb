1fd381c3871b8640a8514fab27863e790eb83871
hadi <hadi_960@hotmail.com>
2021-03-07 00:07:46 -0600

    adds init in database

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -8,7 +8,7 @@
 def can_be_instantiated_and_then_saved
   movie = Movie.new
   movie.title = "This is a title."
-  Movie.new
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -20,29 +20,33 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  Movie.new
+  movie = Movie.new(attributes)
+  movie.save
+  movie
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = { title: "Home Alone", release_date: 1990})
   # If no arguments are passed, use default values:
   # title == "Home Alone"
   # release_date == 1990
   
   Movie.create do |m|
-    __
+    m.title = args[:title]
+    m.release_date = args[:release_date]
+    m.save
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  Movie.first
 end
 
 def can_get_the_last_item_in_the_database
-  __
+  Movie.last
 end
 
 def can_get_size_of_the_database
-  __
+  Movie.size
 end
 
 def can_find_the_first_item_from_the_database_using_id
